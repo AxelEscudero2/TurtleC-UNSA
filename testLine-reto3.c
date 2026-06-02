8a7d562b3756f3c5552674787d0251167f502f01
AxelEscudero89 <148493435+AxelEscudero2@users.noreply.github.com>
2026-06-02 00:46:08 -0500

    Update testLine-reto3.c
    se a cambiado el valor por el que se multiplica la profundidad para hallar el color, y se ha modificado para que tienda a un color un poco más verdoso, al menos en la parte de las ramas, también se corrigió el problema de no subir el lápiz en ningún momento

--- testLine-reto3.c
+++ testLine-reto3.c
@@ -5,7 +5,7 @@ void fractalArbol(Turtle *t, float l, int p, float a, float t1, float t2){
   if(p == 0 || l < 5)
     return;
   //es en este cambio donde pasa de colorear simulando un arbol a ir bajando por una escala de grises, siendo las copas de los árboles los puntos más claros
-  turtleSetColor(t, (255 - ((10 * p) % 256)), (255 - ((10 * p) % 256)), (255 - ((10 * p) % 256)));
+  turtleSetColor(t, (255 - ((50 * p) % 256)), (255 - ((40 * p) % 256)), (255 - ((50 * p) % 256)));
   
   turtleForward(t, l);
 
@@ -18,8 +18,10 @@ void fractalArbol(Turtle *t, float l, int p, float a, float t1, float t2){
   turtleRight(t, a);
   fractalArbol(t, l * t2, p - 1, a, t1, t2);
 
+  turtlePenUp(t);
   turtleLeft(t, a);
   turtleBackward(t, l);
+  turtlePenDown(t);
 }
 
 int main(void){
