ade2a7f68853a7fd602ba6c269264545b4f90ef1
AxelEscudero89 <148493435+AxelEscudero2@users.noreply.github.com>
2026-06-02 00:00:04 -0500

    Reto Final 3, árbol con color dinámico
    En esta actualización, el código posee un modificador al color que es llamado en cada llamada que hace la función, dependiendo únicamente por la profundidad en la que se encuentra, siendo que mientras más lejana a la raíz sea más claro será la escala de grises.
    Por la función usada el código se podría empezar a ver "feo" puesto que después de la profundidad 25, ya que llegado a ese punto, se reiniciará el contado empezando de nuevo en un blanco, marcando un umbral claro de manera visual, pareciendo mas que un árbol conectado, un escáner de un radar naval, si se le mira de cierta manera.

---
+++ testLine-reto3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "../turtlec.h"
+
+void fractalArbol(Turtle *t, float l, int p, float a, float t1, float t2){
+  if(p == 0 || l < 5)
+    return;
+  //es en este cambio donde pasa de colorear simulando un arbol a ir bajando por una escala de grises, siendo las copas de los árboles los puntos más claros
+  turtleSetColor(t, (255 - ((10 * profundidad) % 256)), (255 - ((10 * profundidad) % 256)), (255 - ((10 * profundidad) % 256)));
+  
+  turtleForward(t, l);
+
+  turtleLeft(t, a);
+  fractalArbol(t, l * t1, p - 1, a, t1, t2);
+
+  turtleRight(t, a);
+  fractalArbol(t, l * (t1 + t2)/2.0, p - 1, a, t1, t2);
+
+  turtleRight(t, a);
+  fractalArbol(t, l * t2, p - 1, a, t1, t2);
+
+  turtleLeft(t, a);
+  turtleBackward(t, l);
+}
+
+int main(void){
+  TurtleApp *app = turtleAppCreate(800, 800, "Test Line");
+
+  if(app == NULL)
+    return 1;
+
+  Turtle *t = turtleAppGetTurtle(app);
+  turtleLeft(t, 90.0);
+  fractalArbol(t, 80.0f, 5, 60.0, 0.5, 0.6);
+
+  turtleAppRun(app);
+  turtleAppDestroy(app);
+  return 0;
+}
